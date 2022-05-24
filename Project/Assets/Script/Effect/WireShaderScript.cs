7677dad7c957a496817397e4f40563d10504c855
vinvinvidal <vinvinvidal@gmail.com>
2022-05-24 19:47:17 +0900

    m

---
+++ Project/Assets/Script/Effect/WireShaderScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireShaderScript : GlobalClass
+{
+	//マテリアル
+	Material WireMaterial;
+
+    void Start()
+    {
+		//マテリアル取得
+		WireMaterial = GetComponent<Renderer>().material;
+	}
+
+	void Update()
+    {
+		WireMaterial.SetTextureOffset("_MainTexture", new Vector2(Time.time * 2, 0));
+	}
+}
